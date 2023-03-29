5058b4a50edaff3bb80e8c83e618f25d66f26bb8
GitDidrik <lindberg.didrik@gmail.com>
2023-03-29 12:48:47 -0700

    user choice prompt and computer choice

--- java.js
+++ java.js
@@ -10,16 +10,22 @@ var AcceptedValues = ["R", "P", "S"];
 var userChoice = window.prompt("Welcome to Rock, Paper, Scissors! Please enter R, P, or S ");
 
 
-//declare computerChoice fucniton
-function ComputerChoice() {
-
-}
-
 
 //getrndm function
-function getRndNum(length) {
-    return Math.floor(Math.random() * length);
-  }
+// function ComputerChoice(length) {
+//     return Math.floor(Math.random() * length);
+//   }
+
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+    computerChoice = "Rock";
+} 
+else if(computerChoice <= 0.67) {
+    computerChoice = "Paper";
+} 
+else {
+    computerChoice = "Scissors";
+}
 
 // for loop that checks if user input is either R, P, or S
 for (var i = 0; i < AcceptedValues.length; i++) {
@@ -30,4 +36,50 @@ for (var i = 0; i < AcceptedValues.length; i++) {
     else {
         console.log("Please enter R, P, or S");
     }
+}
+
+// if statement that checks if user input is R, P, or S
+if (userChoice === "R") {
+    window.alert("You chose Rock");
+    var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+    computerChoice = "Rock";
+} 
+else if(computerChoice <= 0.67) {
+    computerChoice = "Paper";
+} 
+else {
+    computerChoice = "Scissors";
+}
+    window.alert("The computer chose " + computerChoice);
+}
+
+if (userChoice === "P") {
+    window.alert("You chose Rock");
+    var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+    computerChoice = "Rock";
+} 
+else if(computerChoice <= 0.67) {
+    computerChoice = "Paper";
+} 
+else {
+    computerChoice = "Scissors";
+}
+    window.alert("The computer chose " + computerChoice);
+}
+
+if (userChoice === "S") {
+    window.alert("You chose Rock");
+    var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+    computerChoice = "Rock";
+} 
+else if(computerChoice <= 0.67) {
+    computerChoice = "Paper";
+} 
+else {
+    computerChoice = "Scissors";
+}
+    window.alert("The computer chose " + computerChoice);
 }
